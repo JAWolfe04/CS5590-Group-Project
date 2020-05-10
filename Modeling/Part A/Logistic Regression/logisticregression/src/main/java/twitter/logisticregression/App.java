6bdb51a923a9e154c5a98174fce2ed51e7278bac
Jonathan <jawhf4@mail.umkc.edu>
2020-05-09 21:18:51 -0500

    Added screenshot of results and changed to Binary classification from multiclassification

--- Modeling/Part A/Logistic Regression/logisticregression/src/main/java/twitter/logisticregression/App.java
+++ Modeling/Part A/Logistic Regression/logisticregression/src/main/java/twitter/logisticregression/App.java
@@ -12,7 +12,7 @@ import org.apache.spark.ml.classification.NaiveBayes;
 import org.apache.spark.ml.classification.NaiveBayesModel;
 import org.apache.spark.ml.classification.RandomForestClassificationModel;
 import org.apache.spark.ml.classification.RandomForestClassifier;
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator;
 import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.sql.Dataset;
@@ -79,55 +79,25 @@ public class App
         
         Dataset<Row> gbtPredictions = gbtModel.transform(testData);
         
-        // Test Error Calculations
-        MulticlassClassificationEvaluator accEvaluator = new MulticlassClassificationEvaluator()
+        // AUC Calculations
+        BinaryClassificationEvaluator AUCEvaluator = new BinaryClassificationEvaluator()
       		  .setLabelCol("label")
-      		  .setPredictionCol("prediction")
-      		  .setMetricName("accuracy");
-        
-        MulticlassClassificationEvaluator f1Evaluator = new MulticlassClassificationEvaluator()
-        		  .setLabelCol("label")
-        		  .setPredictionCol("prediction")
-        		  .setMetricName("f1");
-        
-        MulticlassClassificationEvaluator precEvaluator = new MulticlassClassificationEvaluator()
-      		  .setLabelCol("label")
-      		  .setPredictionCol("prediction")
-      		  .setMetricName("weightedPrecision");
-        
-        MulticlassClassificationEvaluator recEvaluator = new MulticlassClassificationEvaluator()
-      		  .setLabelCol("label")
-      		  .setPredictionCol("prediction")
-      		  .setMetricName("weightedRecall");
-      	
+      		  .setRawPredictionCol("prediction")
+      		  .setMetricName("areaUnderROC");
+
         System.out.println("Logistic Regression Model:");
-        System.out.println("Test Error = " + (1.0 - accEvaluator.evaluate(logPredictions)));
-        System.out.println("F1 = " + f1Evaluator.evaluate(logPredictions));
-        System.out.println("Precision = " + precEvaluator.evaluate(logPredictions));
-        System.out.println("Recall = " + recEvaluator.evaluate(logPredictions));
+        System.out.println("AUC = " + AUCEvaluator.evaluate(logPredictions));
       
         System.out.println("\nDecision Tree Model:");
-        System.out.println("Test Error = " + (1.0 - accEvaluator.evaluate(dtPredictions)));
-        System.out.println("F1 = " + f1Evaluator.evaluate(dtPredictions));
-        System.out.println("Precision = " + precEvaluator.evaluate(dtPredictions));
-        System.out.println("Recall = " + recEvaluator.evaluate(dtPredictions));
+        System.out.println("AUC = " + AUCEvaluator.evaluate(dtPredictions));
         
         System.out.println("\nRandom Forest Model:");
-        System.out.println("Test Error = " + (1.0 - accEvaluator.evaluate(rfPredictions)));
-        System.out.println("F1 = " + f1Evaluator.evaluate(rfPredictions));
-        System.out.println("Precision = " + precEvaluator.evaluate(rfPredictions));
-        System.out.println("Recall = " + recEvaluator.evaluate(rfPredictions));
+        System.out.println("AUC = " + AUCEvaluator.evaluate(rfPredictions));
         
         System.out.println("\nNaive Bayes Model:");
-        System.out.println("Test Error = " + (1.0 - accEvaluator.evaluate(nbPredictions)));
-        System.out.println("F1 = " + f1Evaluator.evaluate(nbPredictions));
-        System.out.println("Precision = " + precEvaluator.evaluate(nbPredictions));
-        System.out.println("Recall = " + recEvaluator.evaluate(nbPredictions));
+        System.out.println("AUC = " + AUCEvaluator.evaluate(nbPredictions));
         
         System.out.println("\nGradient-Boosted Tree Model:");
-        System.out.println("Test Error = " + (1.0 - accEvaluator.evaluate(gbtPredictions)));
-        System.out.println("F1 = " + f1Evaluator.evaluate(gbtPredictions));
-        System.out.println("Precision = " + precEvaluator.evaluate(gbtPredictions));
-        System.out.println("Recall = " + recEvaluator.evaluate(gbtPredictions));
+        System.out.println("AUC = " + AUCEvaluator.evaluate(gbtPredictions));
     }
 }
